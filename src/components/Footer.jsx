8ad152d373f34dcee6448be5293c2f275d9533f0
MIS-Sr.Designer-Iftekharuddin Faisal <Iftekharuddin@aci-bd.com>
2026-06-14 14:18:07 +0600

    test

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="py-5 text-center text-[12px] text-ink-muted">
-      Copyright ©2026
-    </footer>
-  );
-}
--- src/data/user.js
+++
@@ -1,6 +0,0 @@
-export const CURRENT_USER = {
-  name: "Milan Hossain",
-  staffId: "33355",
-  role: "Admin",
-  online: true,
-};
